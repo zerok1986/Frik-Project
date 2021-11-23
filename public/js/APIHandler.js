70bd898f87836df23fc2410492e2cd5d5319f526
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-23 10:43:09 +0100

    Add link to each comic id. Refactor last APIHandler routes

--- public/js/APIHandler.js
+++ public/js/APIHandler.js
@@ -1,10 +1,8 @@
 class APIHandler {
-    constructor(baseUrl) {
-      this.BASE_URL = baseUrl
-      this.axiosApp = axios.create({ baseURL: this.BASE_URL })
-    }
-
-    getVolumes = (query) => axios.post(`/api/search`, {query}).then(res => res)
-//https://comicvine.gamespot.com/api/search/?api_key=8e51b7f7f1fdc9fde6accd0952519a7395de6931&format=json&sort=name:asc&resources=volume&query=${query}&limit=10
-    // getOneRegister = (id) => this.axiosApp.get(`/characters/${id}`)
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl
+    this.axiosApp = axios.create({ baseURL: this.BASE_URL })
   }
+  getVolumes = (query) =>
+    this.axiosApp.post(`/api/search`, { query }).then((res) => res)
+}
